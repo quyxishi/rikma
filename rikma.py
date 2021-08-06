a9b35695d71ee2e5cc77981d7cb5862f6bf171dd
swidelip <78678868+swidelip@users.noreply.github.com>
2021-08-06 09:59:57 +0300

    Update rikma.py

--- rikma.py
+++ rikma.py
@@ -5,7 +5,7 @@ import sys
 import humanize
 import time
 
-bufferSize = 512 * 1024
+bufferSize = 128 * 1024
 result = 0
 
 rikmaLogo = '''
@@ -30,17 +30,17 @@ def encrypt(path):
         try:
             if os.path.isfile(os.path.join(path, name)):
                 if name != 'desktop.ini' and '.aes' not in name:
-                    print('\nEncrypting: ', name)
-                    print('Path: ', os.path.join(path, name))
-                    print('Size: ', getSize(os.path.join(path, name)))
+                    print('\nEncrypting:         ', name)
+                    print('Path:               ', os.path.join(path, name))
+                    print('Size:               ', getSize(os.path.join(path, name)))
                     pyAesCrypt.encryptFile(os.path.join(path, name),
                                            os.path.join(path, name + '.aes'),
                                            key,
                                            bufferSize)
-                    print('Encrypted name: ', name + '.aes')
-                    print('Encrypted path: ', os.path.join(path, name + '.aes'))
-                    print('Encrypted size: ', getSize(os.path.join(path, name + '.aes')))
-                    print('Key: ', key)
+                    print('Encrypted name:     ', name + '.aes')
+                    print('Encrypted path:     ', os.path.join(path, name + '.aes'))
+                    print('Encrypted size:     ', getSize(os.path.join(path, name + '.aes')))
+                    print('Key:                 "'+key+'"')
                     os.remove(os.path.join(path, name))
                     result += 1
             else:
@@ -57,17 +57,17 @@ def decrypt(path):
         try:
             if os.path.isfile(os.path.join(path, name)):
                 if '.aes' in name:
-                    print('\nDecrypting ', name)
-                    print('Path: ', os.path.join(path, name))
-                    print('Size: ', getSize(os.path.join(path, name)))
+                    print('\nDecrypting          ', name)
+                    print('Path:               ', os.path.join(path, name))
+                    print('Size:               ', getSize(os.path.join(path, name)))
                     pyAesCrypt.decryptFile(os.path.join(path, name),
                                            os.path.join(path, name.replace('.aes', '')),
                                            key,
                                            bufferSize)
-                    print('Decrypted name: ', name.replace('.aes', ''))
-                    print('Decrypted path: ', os.path.join(path, name.replace('.aes', '')))
-                    print('Decrypted size: ', getSize(os.path.join(path, name.replace('.aes', ''))))
-                    print('Key: ', key)
+                    print('Decrypted name:     ', name.replace('.aes', ''))
+                    print('Decrypted path:     ', os.path.join(path, name.replace('.aes', '')))
+                    print('Decrypted size:     ', getSize(os.path.join(path, name.replace('.aes', ''))))
+                    print('Key:                 "'+key+'"')
                     os.remove(os.path.join(path, name))
                     result += 1
             else:
@@ -79,50 +79,68 @@ def decrypt(path):
 
 if __name__ == '__main__':
     if '-e' in str(sys.argv) or '-d' in str(sys.argv) or '--encrypt' in str(sys.argv) or '--decrypt' in str(sys.argv):
-        print(rikmaLogo)
-        if '-e' in str(sys.argv) or '--encrypt' in str(sys.argv):
-            pathq = input('Path, example C:\\Users\\Admin\\Desktop:\n> ')
-            passq = input('Key, leave empty if you want to generate key:\n> ')
+        try:
+            print(rikmaLogo)
+            if '-e' in str(sys.argv) or '--encrypt' in str(sys.argv):
+                pathq = input('Path, example C:\\Users\\Admin\\Desktop:\n> ')
 
-            if passq == ' ' or passq == '':
-                lengthq = input('Length of generated key (4-256):\n> ')
+                if not os.path.exists(pathq):
+                    while not os.path.exists(pathq):
+                        pathq = input('Incorrect path, example C:\\Users\\Admin\\Desktop:\n> ')
+                        time.sleep(0.05)
 
-                if 4 <= int(lengthq) <= 257:
-                    lengthq = lengthq
+                passq = input('Key, leave empty if you want to generate key:\n> ')
+
+                if passq == ' ' or passq == '':
+                    lengthq = input('Length of generated key (4-256):\n> ')
+
+                    if 4 <= int(lengthq) <= 257:
+                        lengthq = lengthq
+                    else:
+                        lengthq = '4'
+
+
+                    def random_string_generator(str_size, allowed_chars):
+                        return ''.join(random.choice(allowed_chars) for _ in range(str_size))
+
+
+                    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&-/?@\^_|~0123456789'
+                    size = int(lengthq)
+                    key = random_string_generator(size, chars)
                 else:
-                    lengthq = '4'
+                    key = passq
 
+                starttime = time.time()
+                encrypt(pathq)
+                totaltime = time.time() - starttime
+                print('\nTotal encrypted files: ' + str(result))
+                print('Estimated time: ' + str(totaltime))
+            elif '-d' in str(sys.argv) or '--decrypt' in str(sys.argv):
+                pathq = input('Path, example C:\\Users\\Admin\\Desktop:\n> ')
 
-                def random_string_generator(str_size, allowed_chars):
-                    return ''.join(random.choice(allowed_chars) for _ in range(str_size))
+                if not os.path.exists(pathq):
+                    while not os.path.exists(pathq):
+                        pathq = input('Incorrect path, example C:\\Users\\Admin\\Desktop:\n> ')
+                        time.sleep(0.05)
 
+                passq = input('Key:\n> ')
 
-                chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&-/?@\^_|~0123456789'
-                size = int(lengthq)
-                key = random_string_generator(size, chars)
-            else:
-                key = passq
+                if passq != '' and passq != ' ' and len(passq) >= 0:
+                    key = passq
+                else:
+                    while passq == '' or passq == ' ' or len(passq) <= 0:
+                        passq = input('Enter valid key:\n> ')
+                        time.sleep(0.05)
+                    key = passq
 
-            starttime = time.time()
-            encrypt(pathq)
-            totaltime = time.time() - starttime
-            print('\nTotal encrypted files: ' + str(result))
-            print('Estimated time: ' + str(totaltime))
-        elif '-d' in str(sys.argv) or '--decrypt' in str(sys.argv):
-            pathq = input('Path, example C:\\Users\\Admin\\Desktop:\n> ')
-            passq = input('Key:\n> ')
-
-            if passq != '' or passq != '':
-                print(passq)
-                key = passq
-            else:
-                print('Enter valid key:\n> ')
-
-            starttime = time.time()
-            decrypt(pathq)
-            totaltime = time.time() - starttime
-            print('\nTotal decrypted files: ' + str(result))
-            print('Estimated time: ' + str(totaltime))
+                starttime = time.time()
+                decrypt(pathq)
+                totaltime = time.time() - starttime
+                print('\nTotal decrypted files: ' + str(result))
+                print('Estimated time: ' + str(totaltime))
+        except KeyboardInterrupt:
+            print('\nKeyboardInterrupt')
+            quit()
     else:
         if '-h' in str(sys.argv) or '--help' in str(sys.argv):
             print('''
